1a4f41f32f03b1aed5c3d46317afd654fe655d44
Denis Axenov <denis_axenov@pm.me>
2024-10-02 21:19:44 +0500

    Changed github action

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,29 +3,30 @@ import path from "node:path";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-      head: {
-          link: [
-              {
-                  rel: "icon",
-                  type: "image/png",
-                  href: "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTIiIGhlaWdodD0iMTIiIHZpZXdCb3g9IjAgMCAxMiAxMiIgZmlsbD0ibm9uZSI+PC9zdmc+"
-              }
-          ]
-      }
-  },
-  build: {
-      transpile: [
-          'shiki'
-      ],
-  },
-  css: [
-      '~/assets/global.scss'
-  ],
-  devtools: {
-      enabled: true
-  },
-  srcDir: path.join(__dirname, 'src/docs/'),
-  ssr: false,
-  compatibilityDate: "2024-10-01"
+    app: {
+        baseURL: "/scss-toolkit/",
+        head: {
+            link: [
+                {
+                    rel: "icon",
+                    type: "image/png",
+                    href: "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTIiIGhlaWdodD0iMTIiIHZpZXdCb3g9IjAgMCAxMiAxMiIgZmlsbD0ibm9uZSI+PC9zdmc+"
+                }
+            ]
+        }
+    },
+    build: {
+        transpile: [
+            'shiki'
+        ],
+    },
+    css: [
+        '~/assets/global.scss'
+    ],
+    devtools: {
+        enabled: true
+    },
+    srcDir: path.relative(__dirname, 'src/docs'),
+    ssr: false,
+    compatibilityDate: "2024-10-01"
 })
---
+++ src/docs/components/Sidebar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="sidebar">
+    <h2>Functions</h2>
+    <NuxtLink to="/functions">Functions</NuxtLink>
+    <br>
+    <h2>Mixins</h2>
+    <NuxtLink to="/mixins">Mixins</NuxtLink>
+    <br>
+    <h2>Mixins</h2>
+    <NuxtLink to="/mixins/background">background</NuxtLink>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
